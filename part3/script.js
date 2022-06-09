38110d7ea0baa084f584cc517c10fc0639ffb80d
MiniPaul <pauline.taillet@laposte.net>
2022-06-09 22:19:04 +0200

    new message

--- part3/script.js
+++ part3/script.js
@@ -18,3 +18,58 @@ for(var i=0; i<trash.length; i++) {
         // ... (2) je rappelle le chiffre 0 HTML = nombre de mail qui s'affiche
     })
 }
+
+// ADD NEW MAIL
+document.getElementById('btn-add').addEventListener("click",
+    function(){
+        /////// STEP 1 = Recréer le squelette HTML du mail
+        // Création balise principale div (row)
+        var mainDiv = document.createElement("div");
+            mainDiv.className = "row";
+            document.body.appendChild(mainDiv);
+        // Création balise img avatar
+        var image = document.createElement("img");
+            image.className = "avatar";
+            image.src = "avatar-1.jpg";
+            mainDiv.appendChild(image);
+        // Création balise div pour le message
+        var nameMessageDiv = document.createElement("div");
+            mainDiv.appendChild(nameMessageDiv);
+            // Création balise h6
+            var h6 = document.createElement('h6');
+            h6.textContent = "Eric Dupont";
+            nameMessageDiv.appendChild(h6);
+            // Création balise p
+            var content = document.createElement('p');
+            //Message Value
+            content.textContent = document.getElementById('add-message').value;
+            nameMessageDiv.appendChild(content);
+        // Création balise img trash
+        var trash = document.createElement('img');
+        trash.src = "trash.png";
+        trash.className = "trash";
+        mainDiv.appendChild(trash);
+        
+        /////// STEP 2 = Aller chercher le message saisi dans input
+        //Reset message text input
+        document.getElementById('add-message').value = "";
+        
+        /////// STEP 3 = MAJ du compteur du nombre de mails
+        //Count messages
+        var messagesCount = document.getElementsByTagName('p').length;
+        document.getElementById('count').textContent = messagesCount;
+
+        /////// STEP 4 = Activer l'action poubelle de ce nouveau mail
+        //Remove
+        trash.addEventListener("click", 
+            function(){
+                this.parentNode.remove();
+                //Count messages
+                var messagesCount = document.getElementsByTagName('p').length;
+                document.getElementById('count').textContent = messagesCount;
+
+            }
+        )
+    }
+)
+
